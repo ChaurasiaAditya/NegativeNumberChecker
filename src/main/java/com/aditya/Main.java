fc5743c4a81e2ed66819ada28687bc7e99c0c338
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-08-31 14:23:17 +0530

    written the source code and check the negative number in Main.java

--- src/main/java/com/aditya/Main.java
+++ src/main/java/com/aditya/Main.java
@@ -1,7 +1,28 @@
 package com.aditya;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // create an object for NegativeNumberChecker class
+        NegativeNumberChecker negativeNumberChecker = new NegativeNumberChecker();
+
+        // Create a Scanner object to read input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // Read the number from the user
+        System.out.println("Please enter a number to check: ");
+        int number = scanner.nextInt();
+
+        // store the result in a boolean variable
+        boolean isNegativeNumber = negativeNumberChecker.isNegativeNumber(number);
+
+        // print the result
+        System.out.println("Is " + number + " a negative number? " + isNegativeNumber);
+
+        // close the scanner object
+        scanner.close();
+
     }
 }
